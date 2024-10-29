d3b7a4ab7f54355efb52507697494bea388920b2
lm7272 <lewis.a.mead@gmail.com>
2024-10-29 21:43:43 +0000

    initial commit

--- simplicialpy/__init__.py
+++ simplicialpy/__init__.py
@@ -1,4 +1,4 @@
-from simplicialpy import Simplex, SimplicialComplex
+from .simplicialpy import Simplex, SimplicialComplex
 from xgi.core.simplicialcomplex import SimplicialComplex as XGISimplicialComplex
 from networkx.classes.graph import Graph
 from networkx import find_cliques
@@ -13,15 +13,3 @@ def clique_complex_from_list(cliques: list[Simplex]) -> SimplicialComplex:
 def clique_complex_from_nx(graph: Graph) -> SimplicialComplex:
     facets: list[Simplex] = find_cliques(graph)
     return clique_complex_from_list(facets)
-
-if __name__ == "__main__":
-    sc = SimplicialComplex([[1,2,3]])
-    sc2 = SimplicialComplex(sc.k_faces(1))
-    print(sc)
-    print(sc2)
-    betti_numbers = sc.compute_betti_numbers()
-    print(betti_numbers)
-    print(sc2.betti_numbers)
-    sc3: SimplicialComplex = sc.union(SimplicialComplex(SimplicialComplex([[1,2,4]]).k_faces(1)))
-    print(sc3)
-    print(sc3.betti_numbers)
