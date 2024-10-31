dc242dbbe192aa04f9360afc5803e65244650b30
lm7272 <lewis.a.mead@gmail.com>
2024-10-31 22:27:58 +0000

    fix dodgy typevar

--- simplicialpy/__init__.py
+++ simplicialpy/__init__.py
@@ -1,20 +1,22 @@
 from .simplicialpy import SimplicialComplex
-from .types import Edge, Simplex
 
-from xgi.core.simplicialcomplex import SimplicialComplex as XGISimplicialComplex
+from collections.abc import Iterable
+
 from networkx.classes.graph import Graph
 from networkx import find_cliques
+from xgi.core.simplicialcomplex import SimplicialComplex as XGISimplicialComplex
 
 def complex_from_xgi_complex(sc: XGISimplicialComplex) -> SimplicialComplex:
-    facets: list[frozenset[int]] = sc.edges.maximal().members()
-    return SimplicialComplex([list(facet) for facet in facets])
+    mbrs: list[frozenset[int]] = sc.edges.maximal().members()
+    facets = [tuple(facet) for facet in mbrs]
+    return SimplicialComplex(facets=facets)
 
-def clique_complex_from_list(cliques: list[Edge]) -> SimplicialComplex:
+def clique_complex_from_iterable(cliques: Iterable[Iterable[int]]) -> SimplicialComplex:
     return SimplicialComplex(facets=cliques)
 
 def clique_complex_from_nx(graph: Graph) -> SimplicialComplex:
-    facets: list[Simplex[int]] = find_cliques(graph)
-    return clique_complex_from_list(facets)
+    facets: list[list[int]] = list(find_cliques(graph))
+    return clique_complex_from_iterable(facets)
 
 if __name__ == "__main__":
-    clique_complex_from_list([[1,2], [2,3], [1,3]])
+    clique_complex_from_iterable([[1,2], [2,3], [1,3]])
--- simplicialpy/types.py
+++
@@ -1,5 +0,0 @@
-from typing import TypeVar
-
-Vertex = TypeVar("Vertex", bound = int)
-Edge = TypeVar("Edge", bound = tuple[Vertex[int], Vertex[int]])
-Simplex = TypeVar("Simplex", bound = list[Vertex[int]])
