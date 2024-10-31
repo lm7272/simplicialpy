bc8ba10f714cda022469ea1f97f98995a1af66a2
lm7272 <lewis.a.mead@gmail.com>
2024-10-31 22:28:17 +0000

    save

--- simplicialpy/__init__.py
+++ simplicialpy/__init__.py
@@ -6,7 +6,7 @@ from networkx.classes.graph import Graph
 from networkx import find_cliques
 from xgi.core.simplicialcomplex import SimplicialComplex as XGISimplicialComplex
 
-def complex_from_xgi_complex(sc: XGISimplicialComplex) -> SimplicialComplex:
+def simplicial_complex_from_xgi(sc: XGISimplicialComplex) -> SimplicialComplex:
     mbrs: list[frozenset[int]] = sc.edges.maximal().members()
     facets = [tuple(facet) for facet in mbrs]
     return SimplicialComplex(facets=facets)
@@ -17,6 +17,3 @@ def clique_complex_from_iterable(cliques: Iterable[Iterable[int]]) -> Simplicial
 def clique_complex_from_nx(graph: Graph) -> SimplicialComplex:
     facets: list[list[int]] = list(find_cliques(graph))
     return clique_complex_from_iterable(facets)
-
-if __name__ == "__main__":
-    clique_complex_from_iterable([[1,2], [2,3], [1,3]])
